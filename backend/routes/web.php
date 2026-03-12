32720bc3e9fd8cfee552d65ec4c3ed4a27d859b7
auswald <auswaldyk25@gmail.com>
2026-03-13 06:42:19 +0800

    TRYFIX

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/api-test', function() {
+    return response()->json(['message' => 'web route working']);
+});
 /*
 |--------------------------------------------------------------------------
 | Web Routes
