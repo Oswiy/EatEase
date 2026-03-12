5ac4ead0e2630312a0df2fedb0bf2a5d1d334045
auswald <auswaldyk25@gmail.com>
2026-03-13 05:41:25 +0800

    Fixing

--- backend/api/index.php
+++ backend/api/index.php
@@ -2,9 +2,10 @@
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
+// TEMP DEBUG
+echo json_encode([
+    'base_path' => $app->basePath(),
+    'routes_exist' => file_exists($app->basePath('routes/api.php')),
+    'bootstrap_path' => $app->bootstrapPath(),
+]);
+die();
